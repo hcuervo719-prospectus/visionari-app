2e98d3036dfc1ed61fa0d47b686ca7afd054d192
hcuervo719-prospectus <hcuervo719@gmail.com>
2026-03-04 22:31:42 -0500

    Update page.tsx

--- app/[locale]/dashboard/component-2/page.tsx
+++ app/[locale]/dashboard/component-2/page.tsx
@@ -1,7 +1,17 @@
-import { getLocale } from 'next-intl/server';
+import { setRequestLocale } from 'next-intl/server';
 import Component2Content from '@/components/Component2Content';
 
-export default async function Component2Page() {
-  const locale = await getLocale();
+type Props = {
+  params: { locale: string };
+};
+
+export default async function Component2Page({ params: { locale } }: Props) {
+  setRequestLocale(locale);
   return <Component2Content locale={locale} />;
+}
+
+export function generateStaticParams() {
+  return [
+    'es','en','pt','fr','de','it','nl','pl','tr','ja','ko','ru','hi','id','sv','no'
+  ].map((locale) => ({ locale }));
 }
