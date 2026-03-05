dc23c723067361a07440123eea4682b3828b7e42
hcuervo719-prospectus <hcuervo719@gmail.com>
2026-03-04 22:34:53 -0500

    Update page.tsx

--- app/[locale]/dashboard/component-6/page.tsx
+++ app/[locale]/dashboard/component-6/page.tsx
@@ -1,7 +1,17 @@
-import { getLocale } from 'next-intl/server';
+import { setRequestLocale } from 'next-intl/server';
 import Component6Content from '@/components/Component6Content';
 
-export default async function Component6Page() {
-  const locale = await getLocale();
+type Props = {
+  params: { locale: string };
+};
+
+export default async function Component6Page({ params: { locale } }: Props) {
+  setRequestLocale(locale);
   return <Component6Content locale={locale} />;
+}
+
+export function generateStaticParams() {
+  return [
+    'es','en','pt','fr','de','it','nl','pl','tr','ja','ko','ru','hi','id','sv','no'
+  ].map((locale) => ({ locale }));
 }
